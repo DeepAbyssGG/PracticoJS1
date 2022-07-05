8d82c9f9fb9190581f264c904e1fee1a5b38ecf1
Nicolas Gomez Marigliano <rodnicgm@gmail.com>
2022-07-05 16:44:53 -0300

    traspaso a git todo el tp

---
+++ js/ejercicio1al5.js
@@ -0,0 +1,17 @@
+//ventana alert
+alert("un mensaje");
+
+//texto en pantalla
+document.write("<h5>Hello World</h5>");
+
+//suma de una linea
+document.write("Resultado:" + (3 + 5));
+
+//prompt nombre de usuario
+let nombre = prompt("¿Como te llamas usuario?");
+document.write("hola " + nombre);
+
+//suma en varias lineas
+let valor1 = prompt("elija un numero");
+let valor2 = prompt("elija un numero");
+document.write(parseInt(valor1) + parseInt(valor2));
